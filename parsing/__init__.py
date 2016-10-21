2c01a23c4ea653d07d48bb8828e57eb5d0621571
tsmanner <zephrincochrane@gmail.com>
2016-10-21 17:53:55 -0400

    Removed g4 files from parsing/antlrpy so you can point to any grammar file on the system.  ANTLR generated output always goes in there and imports are always done fresh. ANTLR generation was rolled into the Language class.  A user can now instantiate a Language by handing it the config dictionary, it will invoke ANTLR in the constructor and deal with all the setup at that point.

--- parsing/__init__.py
+++ parsing/__init__.py
@@ -1,4 +1,5 @@
 import antlr4
+import importlib
 import itertools
 import json
 import os
@@ -6,46 +7,9 @@ import re
 import subprocess as sp
 
 
-class GenericListener:
-#    @staticmethod
-    def print_node_context(self, ctx):
-        print(ctx)
-
-
-class Language:
-    def __init__(self, grammar):
-        assert grammar["file"].endswith(".g4")
-        self.name = os.path.basename(grammar["file"])[:-3]
-        exec("import antlrpy." + self.name + "Lexer")
-        exec("import antlrpy." + self.name + "Parser")
-        exec("import antlrpy." + self.name + "Listener")
-
-        class Listener(eval("antlrpy." + self.name + "Listener." + self.name + "Listener")):
-            pass
-
-        for rule in grammar["rules"]:
-            entercmd = "Listener.enter" + grammar["rules"][rule].capitalize() +\
-                       " = GenericListener.print_node_context"
-            print(entercmd)
-            exec(entercmd)
-            exitcmd = "Listener.exit" + grammar["rules"][rule].capitalize() + \
-                      " = GenericListener.print_node_context"
-            print(exitcmd)
-            exec(exitcmd)
-
-        self.lexer = eval("antlrpy." + self.name + "Lexer." + self.name + "Lexer")
-        self.parser = eval("antlrpy." + self.name + "Parser." + self.name + "Parser")
-        self.listener = Listener
-
-    def process(self, filename: str):
-        lexer = self.lexer(antlr4.FileStream(filename))
-        tokens = antlr4.CommonTokenStream(lexer)
-        parser = self.parser(tokens)
-        listener = self.listener()
-        tree = parser.expression()
-        walker = antlr4.ParseTreeWalker()
-        walker.walk(listener, tree)
-
+"""
+Setup some module level information
+"""
 config = json.load(open("parser_config"))
 
 antlr_jar = ''
@@ -69,32 +33,60 @@ if antlr_jar == '':
     exit("No antlr jar found!")
 
 
+class GenericListener:
+    @staticmethod
+    def print_node_context(listener, ctx):
+        print(ctx)
+
+
+class Language:
+    def __init__(self, grammar_config: dict):
+        """
+        Take some grammar configuration information and create a language out of it.
+            1.  Make sure we have a valid grammar file
+            2.  Run ANTLR to generate the lexer, parser, and listener python modules
+            3.  Import the ANTLR generated code
+            4.  Assign passthroughs from this instance's listener to the generic listener
+        """
+        assert grammar_config["file"].endswith(".g4")
+        gencmd = ' '.join(['java', '-jar', antlr_jar,
+                           '-Dlanguage=Python3',
+                           '-o', os.path.dirname(__file__) + os.sep + 'antlrpy',
+                           grammar_config['file']])
+        sp.run(gencmd, stdout=sp.PIPE)
+        self.name = os.path.basename(grammar_config["file"])[:-3]
+        importlib.invalidate_caches()  # Force new imports each time
+        exec("import antlrpy." + self.name + "Lexer")
+        exec("import antlrpy." + self.name + "Parser")
+        exec("import antlrpy." + self.name + "Listener")
+
+        class Listener(eval("antlrpy." + self.name + "Listener." + self.name + "Listener")):
+            pass
+
+        for rule in grammar_config["rules"]:
+            entercmd = "Listener.enter" + grammar_config["rules"][rule].capitalize() +\
+                       " = lambda x,y: print('enter_" + grammar_config["rules"][rule] + "')"
+            print(entercmd)
+            exec(entercmd)
+            exitcmd = "Listener.exit" + grammar_config["rules"][rule].capitalize() + \
+                      " = lambda x,y: print('exit_" + grammar_config["rules"][rule] + "')"
+            print(exitcmd)
+            exec(exitcmd)
+
+        self.lexer = eval("antlrpy." + self.name + "Lexer." + self.name + "Lexer")
+        self.parser = eval("antlrpy." + self.name + "Parser." + self.name + "Parser")
+        self.listener = Listener
+
+    def process(self, filename: str):
+        lexer = self.lexer(antlr4.FileStream(filename))
+        tokens = antlr4.CommonTokenStream(lexer)
+        parser = self.parser(tokens)
+        # TODO: This is the broken step... Figure out how to tell it which rule to start with
+        parser.expression()
+#        walker = antlr4.ParseTreeWalker()
+#        walker.walk(self.listener(), parser.expression())
+
+
 for grammar_name in config['grammars']:
-    grammar = config['grammars'][grammar_name]
-    gencmd = ' '.join(['java', '-jar', antlr_jar,
-                       '-Dlanguage=Python3',
-                       '-o', os.path.dirname(grammar['file']),
-                       grammar['file']])
-#    print(gencmd)
-    proc = sp.run(gencmd, stdout=sp.PIPE)
-#    print(proc)
-#    print(proc.stdout)
-    l = Language(grammar)
+    l = Language(config['grammars'][grammar_name])
     l.process("..\\L2LsuDrvr.h")
-
-#    import parsing.CPP14Lexer
-#    import parsing.CPP14Parser
-#    import parsing.CPP14Listener
-
-#    lexer = parsing.CPP14Lexer.CPP14Lexer(antlr4.FileStream("..\\L2LsuDrvr.h"))
-#    tokens = antlr4.CommonTokenStream(lexer)
-#    parser = parsing.CPP14Parser.CPP14Parser(tokens)
-
-#    class ThisListener(parsing.CPP14Listener.CPP14Listener):
-#        def enterClassname(self, ctx):
-#            GenericListener.print_node_context(ctx)
-
-#    listener = ThisListener()
-#    tree = parser.expression()
-#    walker = antlr4.ParseTreeWalker()
-#    walker.walk(listener, tree)
